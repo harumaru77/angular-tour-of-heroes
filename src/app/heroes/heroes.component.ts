8a263ae49231fb3e4770e0d794ef5973f1388d77
harumaru77 <kwonwoo77@gmail.com>
2017-05-19 10:49:58 +0900

    commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../shared/model/hero.model';
 import {HeroService} from '../shared/service/hero.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'heroes',
@@ -10,7 +11,10 @@ import {HeroService} from '../shared/service/hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.heroes = this.heroService.getHeroes();
@@ -18,5 +22,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
